d80aad6b9d28de83cb6f4af81b90e8abf710c58c
UXEergashev <uxeergashev@example.com>
2026-02-21 23:52:52 +0500

    Configure for Vercel MPA and move to ish_top3

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        worker: resolve(__dirname, 'worker-dashboard.html'),
+        employer: resolve(__dirname, 'employer-dashboard.html'),
+      }
+    }
+  }
 })
